629513b5d31b4615a6ca6749bd0990ccfb7c8b66
Naraveni Akhil <naraveniakhil@email.com>
2023-02-11 23:17:05 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+let contEl = document.getElementById("container")
+console.log(contEl.childNodes)
+const nodeList = document.body;
+console.log(nodeList.childNodes[2].parentNode)
+console.log(nodeList.children)
+console.log(nodeList.children[1].childNodes)
+console.log(nodeList.children[1].children[0].childNodes[0].parentElement)
+console.log(nodeList.children[1].children[0].childNodes[0].parentNode)
